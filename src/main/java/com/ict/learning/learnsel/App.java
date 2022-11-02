c540750abe203ff8b0be8f9d43c9f30fa059f74c
technaveenks <tech.naveenks@gmail.com>
2022-11-03 03:43:57 +0530

    initial commit

---
+++ src/main/java/com/ict/learning/learnsel/App.java
@@ -0,0 +1,13 @@
+package com.ict.learning.learnsel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
