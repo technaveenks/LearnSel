062f36da03ea068d02207cba1070c42491c54125
technaveenks <tech.naveenks@gmail.com>
2022-11-06 02:14:24 +0530

    Update HomeTests.java

--- src/test/java/com/ict/learning/learnsel/tests/HomeTests.java
+++ src/test/java/com/ict/learning/learnsel/tests/HomeTests.java
@@ -1,5 +1,6 @@
 package com.ict.learning.learnsel.tests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,10 +8,16 @@ public class HomeTests {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\navee\\Projects\\LearnSel\\lib\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.amazon.com");
-		driver.manage().window().fullscreen();
+		driver.get("http://automationpractice.com/index.php");
+		driver.manage().window().maximize();
 		String title = driver.getTitle();
 		System.out.println(title);
+		driver.findElement(By.id("search_query_top")).sendKeys("T Shirts");
+		driver.findElement(By.name("submit_search")).click();
+		
+		String dressName = driver.findElement
+				(By.xpath("//*[@id='center_column']/ul/li/div/div[2]/h5/a")).getText();
+		System.out.println(dressName);
 	}
 
 }
